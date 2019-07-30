cb23c6639b6d0d92c2ce5bae480ed2b52f7b2f64
santoshkrishna53 <35844725+santoshkrishna53@users.noreply.github.com>
2019-07-31 00:05:54 +0530

    added register page angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatBadgeModule} from '@angular/material/badge';
+import { RegisterComponent } from './register/register.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatBadgeModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatInputModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
